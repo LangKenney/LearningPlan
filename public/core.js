5cecbcb854b84bca8954b53d22a68c3d2e01b519
Lang Kenney <Langk411@yahoo.com>
2016-07-11 22:28:36 +0000

    Updating html pages

--- public/core.js
+++ public/core.js
@@ -1,39 +1,48 @@
-var todoApp = angular.module('todoApp', ['ngRoute']);
+var toLearnApp = angular.module('toLearnApp', ['ngRoute','ngAnimate', 'ui.bootstrap']);
 
-todoApp.config(function($routeProvider){
+// Model
+toLearnApp.config(function($routeProvider){
     
     $routeProvider
+    
     .when('/',{
-        templateUrl: 'pages/todo.html',
+        templateUrl: 'pages/bio.html',
+        controller: 'CarouselCtrl'
+    })
+    
+    .when('/learning',{
+        templateUrl: 'pages/tolearn.html',
         controller: 'mainController'
     })
     
-    .when('/tobuy',{
-        templateUrl: 'pages/tobuy.html',
+    .when('/projects',{
+        templateUrl: 'pages/projects.html',
         controller: 'secondController'
         
     })
     
 });
 
+//Controllers
 //function mainController($scope, $http){ }
-todoApp.controller('mainController', ['$scope', '$http', function($scope,$http){
-    $http.get('/api/todos')
+toLearnApp.controller('mainController', ['$scope', '$http', function($scope,$http){
+    $http.get('/api/tolearn')
         .success(function(data){
-            $scope.todos = data;
-            console.log('---From view todos---');
+            $scope.tolearn = data;
+            console.log('---From view to learn---');
             console.log(data);
         })
         .error(function(data){
             console.log('Error:'+data);
         });
         
-        $scope.createTodo = function(){
-            $http.post('/api/todos', $scope.formData)
+        //Create a new to learn
+        $scope.createtolearn = function(){
+            $http.post('/api/tolearn', $scope.formData)
                 .success(function(data){
                     $scope.formData = {};
-                    $scope.todos = data;
-                    console.log('---From created todo---');
+                    $scope.tolearn = data;
+                    console.log('---From created to learn---');
                     console.log(data);
                 })
                 .error(function(data){
@@ -41,12 +50,12 @@ todoApp.controller('mainController', ['$scope', '$http', function($scope,$http){
                 });
         };
         
-        //Delete a to-do after checking it
-        $scope.deleteTodo = function(id){
-            $http.delete('/api/todos/'+id)
+        //Delete a to-learn after checking it
+        $scope.deletetolearn = function(id){
+            $http.delete('/api/tolearn/'+id)
                 .success(function(data){
-                    $scope.todos = data;
-                    console.log('---From deleted todo---');
+                    $scope.tolearn = data;
+                    console.log('---From deleted tolearn---');
                     console.log(data);
                 })
                 .error(function(data){
@@ -56,23 +65,26 @@ todoApp.controller('mainController', ['$scope', '$http', function($scope,$http){
             
 }]);
 
-todoApp.controller('secondController', ['$scope', '$http', function($scope,$http){
-        $http.get('/api/tobuys')
+toLearnApp.controller('secondController', ['$scope', '$http', function($scope,$http){
+  
+      $http.get('/api/projects')
         .success(function(data){
-            $scope.tobuys = data;
-            console.log('---From view to buys---');
-            console.log(data);
+            $scope.projects = data;
+
+            console.log('---From view projects---');
+
         })
         .error(function(data){
             console.log('Error:'+data);
         });
         
-        $scope.createTobuy = function(){
-            $http.post('/api/tobuys', $scope.formData)
+        //Create a new to learn
+        $scope.createProject = function(){
+            $http.post('/api/projects', $scope.formData)
                 .success(function(data){
                     $scope.formData = {};
-                    $scope.tobuys = data;
-                    console.log('---From created to buys---');
+                    $scope.projects = data;
+                    console.log('---From created project---');
                     console.log(data);
                 })
                 .error(function(data){
@@ -80,17 +92,94 @@ todoApp.controller('secondController', ['$scope', '$http', function($scope,$http
                 });
         };
         
-        //Delete a to-do after checking it
-        $scope.deleteTobuy = function(id){
-            $http.delete('/api/tobuys/'+id)
+        //Delete a to-learn after checking it
+        $scope.deleteProject = function(id){
+            $http.delete('/api/projects/'+id)
                 .success(function(data){
-                    $scope.tobuys = data;
-                    console.log('---From deleted to buys---');
+                    $scope.projects = data;
+                    console.log('---From deleted project---');
                     console.log(data);
                 })
                 .error(function(data){
                     console.log('Error:'+data);
                 });
         };
-            
+        
+  
+}]);
+
+toLearnApp.controller('CarouselCtrl', ['$scope', function($scope){
+
+    $scope.myInterval = 4000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    var slides = $scope.slides = [];
+    var currIndex = 0;
+    var numberOfSlides = 6;
+    
+    $scope.addSlide = function() {
+        var newWidth = 600 + slides.length + 1;
+        slides.push({
+        image: [
+            'https://unsplash.it/1400/1200?image=883',
+            'https://unsplash.it/1400/1200?image=908',
+            'https://unsplash.it/1400/1200?image=871',
+            'https://unsplash.it/1400/1200?image=826',
+            'https://unsplash.it/1400/1200?image=1040',
+            'https://unsplash.it/1400/1200?image=1030'
+        ][slides.length % numberOfSlides],
+        //   image: 'http://lorempixel.com/' + newWidth + '/300',
+        text: [
+            'Born in Telluride Colorado',
+            'See Projects page for links to examples projects as well as Github',
+            'BS: Aerocpace Engineer Sciences University of Colorado Boulder',
+            'Lead Project intergrater at Lockheed Martin Space Systems Company',
+            'Senior Aerospace Systems Engineer',
+            'See learing Page to see my current learning project'
+        ][slides.length % numberOfSlides],
+        id: currIndex++
+        });
+      };
+    
+    
+    for (var i = 0; i < numberOfSlides; i++) {
+        $scope.addSlide();
+      }
+    
+   $scope.randomize = function() {
+        var indexes = generateIndexesArray();
+        assignNewIndexesToSlides(indexes);
+      };
+
+      // Randomize logic below
+    
+      function assignNewIndexesToSlides(indexes) {
+        for (var i = 0, l = slides.length; i < l; i++) {
+          slides[i].id = indexes.pop();
+        }
+      }
+    
+      function generateIndexesArray() {
+        var indexes = [];
+        for (var i = 0; i < currIndex; ++i) {
+          indexes[i] = i;
+        }
+        return shuffle(indexes);
+      }
+    
+      // http://stackoverflow.com/questions/962802#962890
+   function shuffle(array) {
+        var tmp, current, top = array.length;
+    
+        if (top) {
+          while (--top) {
+            current = Math.floor(Math.random() * (top + 1));
+            tmp = array[current];
+            array[current] = array[top];
+            array[top] = tmp;
+          }
+        }
+    
+        return array;
+      }
 }]);
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var methodOverride = require("method-override");
 //mongoose.connect("mongodb://localhost/to-do");
 
 // V - defined enviromental variable DATABASEURL - on HEROKU settings -> Config Vars
-var url = process.env.DATABASEURL || "mongodb://localhost/to-do";
+var url =  "mongodb://localhost/learning-plan";
 mongoose.connect(url);
 //mongoose.connect("mongodb://lang:_____@ds011775.mlab.com:11775/to-do" || "mongodb://localhost/to-do");
 
@@ -26,125 +26,130 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(methodOverride());
 
-//-----------TO DO-----------
+//-----------TO learn-----------
 //Define mongoose schema - 
-var schema = new mongoose.Schema({ text: 'string' });
+var learningSchema = new mongoose.Schema({ text: 'string' });
 
 //Model
-var Todo = mongoose.model('Todo', schema);
+var Tolearn = mongoose.model('Tolearn', learningSchema);
 
 // V - old code no defined schema
-// var Todo = mongoose.model('Todo', {
+// var tolearn = mongoose.model('tolearn', {
 //     text: String
 // });
 
-//get all todos
+//get all tolearns
 // V - Express application method - app.get(path, callback [, callback ...]) -http://expressjs.com/en/api.html#app.get
-app.get('/api/todos', function(req, res){
+app.get('/api/tolearn', function(req, res){
     // V- mongoose - Model.find(conditions, [projection], [options], [callback]) http://mongoosejs.com/docs/api.html#model_Model.find
-    Todo.find(function(err, todos){
+    Tolearn.find(function(err, tolearn){
         // V - javascript error handling
         if (err){
             // V - express responce method - res.send([body])
             res.send(err);
         }
         // V - express responce method - res.json([body])
-        res.json(todos);
+        res.json(tolearn);
     });
 });
-//create a to-do and send it back
-app.post('/api/todos', function(req, res){
-    console.log('Headers: '+req.complete)
-    Todo.create({
+//create a to-learn and send it back
+app.post('/api/tolearn', function(req, res){
+    Tolearn.create({
         text: req.body.text,
         done: false
-    }, function(err, todos){
+    }, function(err, tolearn){
         if (err){
             res.send(err);
         } 
-        Todo.find(function(err, todos){
+        Tolearn.find(function(err, tolearn){
             if (err){
                 res.send(err);
             }
-            res.json(todos);
+            res.json(tolearn);
         });
     });
 });
 
-//Delete a To-Do
-app.delete('/api/todos/:todo_id', function(req, res){
-    Todo.remove({
-        _id : req.params.todo_id
-    }, function(err, todo){
+//Delete a To-learn
+app.delete('/api/tolearn/:tolearn_id', function(req, res){
+    Tolearn.remove({
+        _id : req.params.tolearn_id
+    }, function(err, tolearn){
         if(err){
             res.send(err);
         }
-        Todo.find(function(err, todos){
+        Tolearn.find(function(err, tolearn){
             if (err){
                 res.send(err);
             }
-            res.json(todos);
+            res.json(tolearn);
         });
     });
 });
 
-//-----------TO BUY-----------
+//-----------PROJECTS-----------
+// var projectSchema = new mongoose.Schema({ text: 'string' });
+//Define mongoose schema - 
+var projectSchema = new mongoose.Schema({ 
+    projectName: String,
+    discription: String,
+    link: String
+});
 
 //Model
-var Tobuy = mongoose.model('Tobuy', {
-    text: String
-});
+var Projects = mongoose.model('Projects', projectSchema);
 
-//get all tobuys
-// V-express method - app.get(path, callback [, callback ...])
-app.get('/api/tobuys', function(req, res){
-    // V- javascript method -arr.find(callback[, thisArg])
-    Tobuy.find(function(err, tobuys){
+app.get('/api/projects', function(req, res){
+    // V- mongoose - Model.find(conditions, [projection], [options], [callback]) http://mongoosejs.com/docs/api.html#model_Model.find
+    Projects.find(function(err, projects){
         // V - javascript error handling
         if (err){
             // V - express responce method - res.send([body])
             res.send(err);
         }
         // V - express responce method - res.json([body])
-        res.json(tobuys);
+        res.json(projects);
     });
 });
-
-//create a to-buy and send it back
-app.post('/api/tobuys', function(req, res){
-    Tobuy.create({
-        text: req.body.text,
+//create a to-learn and send it back
+app.post('/api/projects', function(req, res){
+    Projects.create({
+        projectName: req.body.projectName,
+        discription: req.body.discription,
+        link: req.body.link,
         done: false
-    }, function(err, tobuys){
+    }, function(err, project){
         if (err){
             res.send(err);
         } 
-        Tobuy.find(function(err, tobuys){
+        Projects.find(function(err, project){
             if (err){
-                res.send(err)
+                res.send(err);
             }
-            res.json(tobuys);
+            res.json(project);
         });
     });
 });
 
-//Delete a To-Do
-app.delete('/api/tobuys/:tobuy_id', function(req, res){
-    // V - mongoose method - Query#remove([criteria], [callback]) - http://mongoosejs.com/docs/api.html#query_Query-remove
-    Tobuy.remove({
-        _id : req.params.tobuy_id
-    }, function(err, tobuy){
+//Delete a To-learn
+app.delete('/api/projects/:project_id', function(req, res){
+    Projects.remove({
+        _id : req.params.project_id
+    }, function(err, project){
         if(err){
             res.send(err);
         }
-        Tobuy.find(function(err, tobuys){
+        Projects.find(function(err, project){
             if (err){
                 res.send(err);
             }
-            res.json(tobuys);
+            res.json(project);
         });
     });
 });
+
+
+//--------------SENDING THE MAIN INDEX PAGE--------------
 
 //Angular application send!
 app.get('*', function(req, res){
@@ -153,5 +158,5 @@ app.get('*', function(req, res){
 });
 
 app.listen(process.env.PORT,process.env.IP,function(){
-    console.log('Running the To-Do server...');
+    console.log('Running the To-Learn server...');
 });
